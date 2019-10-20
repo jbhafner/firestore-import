d2394a9f24e6b5b6f807c709b60673452b9c8785
Brian Hafner <bhafner@Mac-f40f2434ab72.local>
2019-10-20 06:08:16 -0700

    first commit

---
+++ import.js
@@ -0,0 +1,21 @@
+// Imports
+const firestoreService = require('firestore-export-import');
+const firebaseConfig = require('./config.js');
+const serviceAccount = require('./serviceAccount.json');
+
+// JSON To Firestore
+const jsonToFirestore = async () => {
+  try {
+    console.log('Initialzing Firebase');
+    await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
+    console.log('Firebase Initialized');
+
+    await firestoreService.restore('./six-wolves-export.json');
+    console.log('Upload Success');
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+jsonToFirestore();
